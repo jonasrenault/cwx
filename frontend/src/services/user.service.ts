258fc6150c1cbe70360638074cd97c240f5dc033
Jonas <jonasrenault@gmail.com>
2023-03-16 17:26:06 +0100

    5 deploy app to hosted web server and enable sso for users (#10)
    * Add an admin panel with a button to load fixtures
    
    * fix tsc issues

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -4,22 +4,22 @@ import { User } from '../contexts/auth'
 const API_URL = import.meta.env.VITE_BACKEND_API_URL
 
 class UserService {
-  async getProfile(): User {
+  async getProfile(): Promise<User> {
     const response = await axios.get(API_URL + 'users/me')
     return response.data
   }
 
-  async updateProfile(profile: User): User {
+  async updateProfile(profile: User): Promise<User> {
     const response = await axios.patch(API_URL + 'users/me', profile)
     return response.data
   }
 
-  async updateUser(userId: string, profile: User): User {
+  async updateUser(userId: string, profile: User): Promise<User> {
     const response = await axios.patch(API_URL + `users/${userId}`, profile)
     return response.data
   }
 
-  async getUsers(): Array<User> {
+  async getUsers(): Promise<Array<User>> {
     const response = await axios.get(API_URL + 'users')
     return response.data
   }
--- frontend/src/services/wall.service.ts
+++ frontend/src/services/wall.service.ts
@@ -1,16 +1,16 @@
 import axios from 'axios'
-import { Wall } from '../models/wall'
+import { Wall, Route } from '../models/wall'
 
 const API_URL = import.meta.env.VITE_BACKEND_API_URL
 
 class WallService {
-  async getWalls(fetchRoutes = false): Array<Wall> {
+  async getWalls(fetchRoutes = false): Promise<Array<Wall>> {
     const options = fetchRoutes ? { params: { fetch_routes: 1 } } : {}
     const response = await axios.get(API_URL + 'walls', options)
     return response.data
   }
 
-  async getWall(wallId: string, fetchRoutes = false): Wall {
+  async getWall(wallId: string, fetchRoutes = false): Promise<Wall> {
     const options = fetchRoutes ? { params: { fetch_routes: 1 } } : {}
     const response = await axios.get(API_URL + `walls/${wallId}`, options)
     return response.data
@@ -21,7 +21,7 @@ class WallService {
    * @param wallId
    * @returns
    */
-  async getWallRoutes(wallId: string): Array<Route> {
+  async getWallRoutes(wallId: string): Promise<Array<Route>> {
     const response = await axios.get(API_URL + `walls/${wallId}/routes`)
     return response.data
   }
